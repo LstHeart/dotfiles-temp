69374ffbb0e59f66ad870ff7f04adf8f7cbc3ab5
LstHeart <dennis.lstheart@qq.com>
2022-07-28 19:10:46 +0000

    update

--- install.sh
+++ install.sh
@@ -7,19 +7,19 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 LOG_FILE='dotfiles.log'
 
 sudo chmod -R 755 ./scripts/ \
- && (timeNow && echo "[working-dir]: $DIR, Dotfiles will begin✨")  >$LOG_FILE
+ && (timeNow && echo "Dotfiles will begin✨")  >$LOG_FILE
 echo "[working-dir]: $DIR" \ >>$LOG_FILE
 
 (timeNow && echo "copy scripts") >>$LOG_FILE
-# cp -r $DIR/scripts $HOME
+cp -r $DIR/scripts $HOME
 (timeNow && echo "copy scripts done🎉") >>$LOG_FILE
 
 (timeNow && echo "copy configs") >>$LOG_FILE
-# cp -r $DIR/configs/bash $HOME && cp -r $DIR/configs/fish $HOME/.config/fish
+cp -r $DIR/configs/bash $HOME && cp -r $DIR/configs/fish $HOME/.config/fish
 (timeNow && echo "copy configs done🎉") >>$LOG_FILE
 
 # active profile
-# source ~/.bash_profile
+source ~/.bash_profile
 
 (timeNow && echo 'Dotfiles done🎉') >>$LOG_FILE
 exit
