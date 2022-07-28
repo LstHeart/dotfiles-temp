b842d203553ae79ca90b6bd219a0bb5bb9cd8488
LstHeart <dennis.lstheart@qq.com>
2022-07-28 15:35:56 +0000

    test: close

--- install.sh
+++ install.sh
@@ -11,15 +11,15 @@ sudo chmod -R 755 ./scripts/ \
 echo "[working-dir]: $DIR" \ >>$LOG_FILE
 
 (timeNow && echo "copy scripts") >>$LOG_FILE
-cp -r $DIR/scripts $HOME
+# cp -r $DIR/scripts $HOME
 (timeNow && echo "copy scripts done🎉") >>$LOG_FILE
 
 (timeNow && echo "copy configs") >>$LOG_FILE
-cp -r $DIR/configs/bash $HOME && cp -r $DIR/configs/fish $HOME/.config/fish
+# cp -r $DIR/configs/bash $HOME && cp -r $DIR/configs/fish $HOME/.config/fish
 (timeNow && echo "copy configs done🎉") >>$LOG_FILE
 
 # active profile
-source ~/.bash_profile
+# source ~/.bash_profile
 
 (timeNow && echo 'Dotfiles done🎉') >>$LOG_FILE
 exit
