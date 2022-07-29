510ca6258908127a1bac6ea6b119e6ff40297ed2
LstHeart <dennis.lstheart@qq.com>
2022-07-29 13:37:43 +0800

    fix: cp error

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ cp -afuv $DIR/scripts $HOME
 (timeNow && echo "copy scripts done🎉") >>$LOG_FILE
 
 (timeNow && echo "copy configs") >>$LOG_FILE
-# cp -afuv $DIR/configs/bash/.* $HOME
-cp -afuv $DIR/configs/fish/* $HOME/.config/fish
+cp -afuv $DIR/configs/bash/. $HOME
+cp -afuv $DIR/configs/fish/. $HOME/.config/fish
 (timeNow && echo "copy configs done🎉") >>$LOG_FILE
 
 # active profile
